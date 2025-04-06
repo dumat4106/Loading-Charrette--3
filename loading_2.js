84307ee059b5b75fdf7f4068b607335ca3656218
Matthew Du <johndoe@example.com>
2025-04-06 16:23:39 -0500

    loading 2

--- loading_2.js
+++ loading_2.js
@@ -1,10 +1,12 @@
-let row = 100;
-let col = 100;
+let row = 50;
+let col = 50;
 
 let stripWidth = 500 / col;
 let rowWidth = 500 / row;
 
 let grid = [];
+let originalGrid = [];
+let shuffledGrid = [];
 
 function preload() {
     img = loadImage("ArtworkImages/Artwork_2.jpg");
@@ -17,17 +19,78 @@ function preload() {
     for (let i = 0; i < col; i++) {
         grid[i] = [];
         for (let j = 0; j < row; j++) {
-            grid[i][j] = img.get(i * col, i * row, stripWidth, rowWidth);
+            grid[i][j] = img.get(i * stripWidth, j * rowWidth, stripWidth, rowWidth);
         }
     }
+
+    originalGrid = grid.map(row => row.map(cell => cell.get()));
+
+    shuffledGrid = shuffle2DArray(grid);
     
   }
   
   function draw() {
+      
     for (let i = 0; i < col; i++) {
         for (let j = 0; j < row; j++) {
-            image(grid[i][j], i * stripWidth, j * rowWidth);
+            image(shuffledGrid[i][j], i * stripWidth, j * rowWidth);
+        }
+      }
+  
+  }
+
+  function shuffle2DArray(arr) {
+
+      let flatArray = [];
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+          flatArray.push(arr[i][j]);
+        }
+      }
+    
+      flatArray = shuffle(flatArray);
+    
+      let k = 0;
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+          arr[i][j] = flatArray[k++];
+        }
+      }
+
+      return arr;
+  }
+
+  function mousePressed() {
+    if (col > 6 && row > 6) {
+      col -= 5;
+      row -= 5;
+      generateStrips();
+    }
+    else {
+      col = 50;
+      row = 50;
+      stripWidth = 500 / col;
+      rowWidth = 500 / row;
+      shuffledGrid = originalGrid.map(row => row.map(cell => cell.get()));
+    }
+  }
+  
+  function generateStrips() {
+  
+      stripWidth = 500 / col;
+      rowWidth = 500 / row;
+
+      grid.length = 0;
+    
+      for (let i = 0; i < col; i++) {
+        grid[i] = [];
+        for (let j = 0; j < row; j++) {
+            grid[i][j] = img.get(i * stripWidth, j * rowWidth, stripWidth, rowWidth);
         }
     }
     
+      shuffledGrid = shuffle2DArray(grid);
+    
+    
   }
+  
